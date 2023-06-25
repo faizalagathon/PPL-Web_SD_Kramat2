adb42e0a4b9924a041e5240a414749ce41ddc232
Nurfajari <azisnurfajari@gmail.com>
2023-06-25 17:02:19 +0700

    nambah footer di setiap halaman carousel

--- daftarCarousel/edit_carousel.php
+++ daftarCarousel/edit_carousel.php
@@ -92,73 +92,123 @@ else{
     </div>
   </div> -->
   <!-- akhir navbar kedua -->
-  <form action="aksiedit.php?ParamAksi=ubah_foto&ParamTable=carousel" method="post" enctype="multipart/form-data">
-    <div class="mt-2">
-      <div class="card m-auto" style="width: 30rem;">
-        <input type="hidden" name="gambarLama" value="<?= $carousel[0]['gambarCarousel'] ?>">
-        <input type="hidden" name="idCarousel" value="<?= $carousel[0]['idCarousel'] ?>">
-        <img class="card-img-top" alt="..." src="../assets/imgs/fotocarousel/<?= $carousel[0]['gambarCarousel'] ?>">
-        <div class="card-body">
-          <div class="mb-3">
-            <h5 class="card-title">Edit Gambar Carousel</h5>
-            <small class="card-text" style="color: red;">*Ekstensi yang diperbolehkan .png | .jpg | .jpeg | .gif</small>
-            <input class="form-control mt-4" type="file" name="gambarCarousel" id="foto" required>
+  <div class="container">
+    <div class="my-5">
+      <form action="aksiedit.php?ParamAksi=ubah_foto&ParamTable=carousel" method="post" enctype="multipart/form-data">
+        <div class="mt-2">
+          <div class="card m-auto" style="width: 30rem;">
+            <input type="hidden" name="gambarLama" value="<?= $carousel[0]['gambarCarousel'] ?>">
+            <input type="hidden" name="idCarousel" value="<?= $carousel[0]['idCarousel'] ?>">
+            <img class="card-img-top" alt="..." src="../assets/imgs/fotocarousel/<?= $carousel[0]['gambarCarousel'] ?>">
+            <div class="card-body">
+              <div class="mb-3">
+                <h5 class="card-title">Edit Gambar Carousel</h5>
+                <small class="card-text" style="color: red;">*Ekstensi yang diperbolehkan .png | .jpg | .jpeg | .gif</small>
+                <input class="form-control mt-4" type="file" name="gambarCarousel" id="foto" required>
+              </div>
+              <div style="padding: 5px;">
+                <button type="submit" class="btn btn-warning text-white">Save</button>
+                <a href="modifikasi_carousel.php" class="btn btn-secondary">Kembali</a>
+              </div>
+            </div>
           </div>
-          <div style="padding: 5px;">
-            <button type="submit" class="btn btn-warning text-white">Save</button>
-            <a href="modifikasi_carousel.php" class="btn btn-secondary">Kembali</a>
+        </div>
+    
+        <?php
+        // if ( isset($_POST['hidden']) ){
+    
+        //     $rand = rand();
+        //     $ekstensi =  array('png','jpg','jpeg','gif');
+        //     $filename = $_FILES['foto']['name'];
+        //     $ukuran = $_FILES['foto']['size'];
+        //     $ext = pathinfo($filename, PATHINFO_EXTENSION);
+    
+        //     if ( ($filename && $ukuran) == NULL ){
+        //         echo "
+        //         <script>
+        //         alert('Data Gagal di Edit');
+        //         document.location.href = 'modifikasi_carousel.php';
+        //         </script>
+        //         ";
+    
+        //     } else {
+        //         $xx = $rand.'_'.$filename;
+        //         $sqlU= "UPDATE carousel
+        //         SET
+        //             gambarCarousel='$xx'
+        //         WHERE idCarousel = '$idcarousel'";
+        //         move_uploaded_file($_FILES['foto']['tmp_name'], 'fotocarousel/'.$xx);
+    
+        //         mysqli_query($db_link,$sqlU);
+        //         echo "
+        //         <script>
+        //         alert('Data Berhasil di Edit');
+        //                 document.location.href = 'modifikasi_carousel.php';
+        //         </script>
+        //         ";
+    
+        //     }
+        // }
+    
+        ?>
+      </form>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <!-- SECTION FOOTER -->
+    <div class="footer bg-dark" style="background: url(../assets/imgs/Frame_9.png);background-size: cover;">
+      <div class="row p-5">
+        <div class="col-md-4 p-3">
+          <div class="sd text-center">
+            <a class="navbar-brand p-0" href="home.html">
+                <img src="../assets/imgs/logo_footer.png" alt="Logo" width="200" class="">
+            </a>
+            <p class="text-white fs-6 ms-4">Jangan hanya bisa untuk bermimpi saja, tapi berusaha dan berdoa untuk menggapai mimpinya</p>
+            <!-- SECTION SOSMED -->
+            <div class="ms-4">
+              <a href="https://instagram.com/sdnkramat2kotacirebon?igshid=YmMyMTA2M2Y" class="text-white text-decoration-none me-3 ms-auto">
+                <img src="../assets/imgs/icon/icon_ig_primary.png" width="30px" alt="">
+              </a>
+              <a href="https://www.facebook.com/sdn.kramatdua?mibextid=ZbWKwL" class="text-white text-decoration-none me-3 ms-auto">
+                <img src="../assets/imgs/icon/icon_fb_primary.png" width="30px" alt="">
+              </a>
+              <a href="https://youtube.com/@sdnkramat2cirebon649 " class="text-white text-decoration-none">
+                <img src="../assets/imgs/icon/icon_yt_primary.png" width="30px" alt="">
+              </a>
+            </div>
+            <!-- !SECTION SOSMED -->
+          </div>
+        </div>
+        <div class="col-md-4 p-3 ms-auto">
+          <div class="kontak text-center">
+            <h5 class="text-white mb-4">Contact Us</h5>
+            <p class="text-white">Jl. Siliwangi No. 44Kota Cirebon </p>
+            <p class="text-white">Telp. (0231) 202998</p>
+          </div>
+        </div>
+        <div class="col-md-4 p-3 ms-auto">
+          <div class="guide text-center">
+            <div class="">
+                <div class="">
+                  <h5 class="text-white mb-4">Viewer Guides</h5>
+                </div>
+                <div class="">
+                  <a class="nav-link text-white" aria-current="page" href="../home.php">Home</a>
+                  <a class="nav-link text-white" href="../profile/profile.php">Profil</a>
+                  <a class="nav-link text-white" href="../daftarBerita/berita.php">Berita</a>
+                </div>
+            </div>
           </div>
         </div>
       </div>
+      <div class="text-center">
+        <p class="text-white border-top border-white pb-3 pt-2 mx-3 mb-0 mt-0">
+          Coypright By @SD_Keramat2023
+        </p>
+      </div>
     </div>
-
-    <?php
-    // if ( isset($_POST['hidden']) ){
-
-    //     $rand = rand();
-    //     $ekstensi =  array('png','jpg','jpeg','gif');
-    //     $filename = $_FILES['foto']['name'];
-    //     $ukuran = $_FILES['foto']['size'];
-    //     $ext = pathinfo($filename, PATHINFO_EXTENSION);
-
-    //     if ( ($filename && $ukuran) == NULL ){
-    //         echo "
-    //         <script>
-    //         alert('Data Gagal di Edit');
-    //         document.location.href = 'modifikasi_carousel.php';
-    //         </script>
-    //         ";
-
-    //     } else {
-    //         $xx = $rand.'_'.$filename;
-    //         $sqlU= "UPDATE carousel
-    //         SET
-    //             gambarCarousel='$xx'
-    //         WHERE idCarousel = '$idcarousel'";
-    //         move_uploaded_file($_FILES['foto']['tmp_name'], 'fotocarousel/'.$xx);
-
-    //         mysqli_query($db_link,$sqlU);
-    //         echo "
-    //         <script>
-    //         alert('Data Berhasil di Edit');
-    //                 document.location.href = 'modifikasi_carousel.php';
-    //         </script>
-    //         ";
-
-    //     }
-    // }
-
-    ?>
-  </form>
-  <!-- SECTION FOOTER -->
-  <div class="footer mt-2" style="bottom: 0;">
-    <div class="text-center bg-dark" style="padding: 5%;">
-      <p class="text-white mb-0">
-        Coypright By @SD_Keramat2023
-      </p>
-    </div>
+    <!-- !SECTION FOOTER -->
   </div>
-  <!-- !SECTION FOOTER -->
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
--- daftarCarousel/modifikasi_carousel.php
+++ daftarCarousel/modifikasi_carousel.php
@@ -34,7 +34,7 @@ if (isset($_GET['aksi']) && $_GET['aksi'] == 'hapus') {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
+  <title>Carousel</title>
   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> -->
   <link rel="stylesheet" href="css/modif_carousel.css">
   <link rel="stylesheet" href="../assets/css/bootstrap/bootstrap.min.css">
@@ -119,7 +119,7 @@ if (isset($_GET['aksi']) && $_GET['aksi'] == 'hapus') {
     <div class="mt-4">
       <h3>Data Carousel</h3>
     </div>
-    <div class="tbl-content p-3 border border-1 border-dark mb-2" style="border-radius: 20px;">
+    <div class="tbl-content p-3 border border-1 border-dark mb-5" style="border-radius: 20px;">
       <a class="button-24 btn btn-primary w-100" href="tambah_carousel.php">Tambah</a>
       <div class="d-flex flex-wrap pt-4">
         <?php foreach ($carousel as $c) : ?>
@@ -134,20 +134,63 @@ if (isset($_GET['aksi']) && $_GET['aksi'] == 'hapus') {
           </div>
         <?php endforeach ?>
       </div>
-      <!-- <div class="text-end">
-          <a class="button-24-back btn btn-outline-dark" href="../home.php">Kembali</a>
-        </div> -->
     </div>
   </div>
-  <!-- SECTION FOOTER -->
-  <div class="footer" style="bottom: 0;">
-    <div class="text-center bg-dark" style="padding: 5%;">
-      <p class="text-white mb-0">
-        Coypright By @SD_Keramat2023
-      </p>
+  <div class="container-fluid">
+    <!-- SECTION FOOTER -->
+    <div class="footer bg-dark" style="background: url(../assets/imgs/Frame_9.png);background-size: cover;">
+      <div class="row p-5">
+        <div class="col-md-4 p-3">
+          <div class="sd text-center">
+            <a class="navbar-brand p-0" href="home.html">
+                <img src="../assets/imgs/logo_footer.png" alt="Logo" width="200" class="">
+            </a>
+            <p class="text-white fs-6 ms-4">Jangan hanya bisa untuk bermimpi saja, tapi berusaha dan berdoa untuk menggapai mimpinya</p>
+            <!-- SECTION SOSMED -->
+            <div class="ms-4">
+              <a href="https://instagram.com/sdnkramat2kotacirebon?igshid=YmMyMTA2M2Y" class="text-white text-decoration-none me-3 ms-auto">
+                <img src="../assets/imgs/icon/icon_ig_primary.png" width="30px" alt="">
+              </a>
+              <a href="https://www.facebook.com/sdn.kramatdua?mibextid=ZbWKwL" class="text-white text-decoration-none me-3 ms-auto">
+                <img src="../assets/imgs/icon/icon_fb_primary.png" width="30px" alt="">
+              </a>
+              <a href="https://youtube.com/@sdnkramat2cirebon649 " class="text-white text-decoration-none">
+                <img src="../assets/imgs/icon/icon_yt_primary.png" width="30px" alt="">
+              </a>
+            </div>
+            <!-- !SECTION SOSMED -->
+          </div>
+        </div>
+        <div class="col-md-4 p-3 ms-auto">
+          <div class="kontak text-center">
+            <h5 class="text-white mb-4">Contact Us</h5>
+            <p class="text-white">Jl. Siliwangi No. 44Kota Cirebon </p>
+            <p class="text-white">Telp. (0231) 202998</p>
+          </div>
+        </div>
+        <div class="col-md-4 p-3 ms-auto">
+          <div class="guide text-center">
+            <div class="">
+                <div class="">
+                  <h5 class="text-white mb-4">Viewer Guides</h5>
+                </div>
+                <div class="">
+                  <a class="nav-link text-white" aria-current="page" href="../home.php">Home</a>
+                  <a class="nav-link text-white" href="../profile/profile.php">Profil</a>
+                  <a class="nav-link text-white" href="../daftarBerita/berita.php">Berita</a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-center">
+        <p class="text-white border-top border-white pb-3 pt-2 mx-3 mb-0 mt-0">
+          Coypright By @SD_Keramat2023
+        </p>
+      </div>
     </div>
+    <!-- !SECTION FOOTER -->
   </div>
-  <!-- !SECTION FOOTER -->
   <!-- <script src="../assets/js/bootstrap/bootstrap.min.js"></script> -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     // '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
