01987c9072223526f6e7320f3481f97c18d86897
Nurfajari <azisnurfajari@gmail.com>
2023-06-27 15:58:01 +0700

    perbaikan footer

--- home.php
+++ home.php
@@ -242,7 +242,7 @@ $datacarousel = mysqli_query($link,"SELECT * FROM carousel ORDER BY idCarousel A
                 </div>
                 <div class="col-md-7 ms-auto">
                   <div class="pt-5">
-                    <h3 class="pt-5 mb-3 fw-bold">Sambutan</h3>
+                    <h3 class="mb-3 fw-bold">Sambutan</h3>
                     <p>
                       Selamat datang di halaman Website Resmi Sekolah kami. Website ini dibuat untuk memberikan informasi yang lengkap dan terkini mengenai Kegiatan sekolah, Prestasi Siswa, Visi Misi, dan berbagai berita terkini tentang sekolah. Dan kami berkomitmen untuk memberikan pendidikan berkualitas, menciptakan lingkungan belajar yang inspiratif, serta mengembangkan potensi dan bakat setiap siswa. Kami percaya bahwa setiap individu memiliki keunikan dan kami berupaya untuk membantu mereka tumbuh dan berkembang dengan baik. <br><br>
                       Website ini adalah sumber informasi yang penting bagi orang tua, siswa, dan masyarakat sekitar. Kami mengundang Anda sekalian untuk menjelajahi setiap bagian dari website kami, mempelajari kegiatan dan prestasi yang telah kami dan murid kami capai.
@@ -280,7 +280,7 @@ $datacarousel = mysqli_query($link,"SELECT * FROM carousel ORDER BY idCarousel A
             </div>
             <!-- !SECTION VISI MISI -->
             <!-- SECTION GALERI -->
-            <div class="py-5">
+            <div class="py-5 px-3">
             <div style="scroll-snap-type: y mandatory;">
               <h3 class="text-center">GALERI</h3>
                   <!--SECTION Gambar -->
--- profile/profile.php
+++ profile/profile.php
@@ -131,10 +131,10 @@ $daftarEkskul = query("SELECT * FROM ekskul INNER JOIN guru ON ekskul.idPembimbi
                         <?php endif; ?>
                     </div>
                     <?php if (isset($login) && $login != false) : ?>
-                        <a href="../login/logout.php?halamanAsal=daftar_guru.php" class="nav-link text-white" onclick="return confirm('Yakin ingin Logout dari Admin?')">Logout</a>
+                        <a href="../login/logout.php?halamanAsal=daftar_guru.php" class="ps-3 nav-link text-white" onclick="return confirm('Yakin ingin Logout dari Admin?')">Logout</a>
                     <?php endif; ?>
                     <?php if (isset($login) && $login == false) : ?>
-                        <a href="../login/login.php" class="nav-link text-white">Login Admin</a>
+                        <a href="../login/login.php" class="ps-3 nav-link text-white">Login Admin</a>
                     <?php endif; ?>
                 </div>
             </div>
@@ -281,9 +281,9 @@ $daftarEkskul = query("SELECT * FROM ekskul INNER JOIN guru ON ekskul.idPembimbi
     </div>
     <!-- !SECTION FEEDBACK -->
   </div>
-  <div class="container-fluid">
+  <div class="container-fluid p-0">
     <!-- SECTION FOOTER -->
-    <div class="footer bg-dark" style="background: url(../assets/imgs/Frame_9.png);background-size: cover;">
+    <div class="footer bg-dark">
       <div class="row p-5">
         <div class="col-md-4 p-3">
           <div class="sd text-center">
